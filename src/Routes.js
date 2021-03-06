8283c1f721729fb491fb3e3d6b7e897bb78c8237
sriver7 <rsonny89@gmail.com>
2021-03-05 21:35:37 -0500

    Handling 404 Pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import ErrorPage from "./containers/ErrorPage";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route>
+        <ErrorPage />
       </Route>
     </Switch>
   );
---
+++ src/containers/ErrorPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ErrorPage.css";
+
+export default function ErrorPage() {
+  return (
+    <div className="NotFound text-center">
+      <h3>Error, page not found!</h3>
+    </div>
+  );
+}
