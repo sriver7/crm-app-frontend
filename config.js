0270361fc54193f295b7fdfc894f0fefeeb0dca5
sriver7 <rsonny89@gmail.com>
2021-03-05 21:48:17 -0500

    Adding Amplify Configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://931rt4elb6.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east1",
+    USER_POOL_ID: "us-east-1_LdmKSo0Uz",
+    APP_CLIENT_ID: "6hfm5n9fbgec6npv14aa7nkjfd",
+    IDENTITY_POOL_ID: "us-east-1:bef55dc7-7c3b-4eb3-a319-c000abb15964",
+  },
+};
+
+export default config;
