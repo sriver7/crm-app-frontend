bb86017a95569faf4de1ef7d34b93cfbead21792
sriver7 <rsonny89@gmail.com>
2021-03-05 22:03:35 -0500

    Adding Login Form

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import ErrorPage from "./containers/ErrorPage";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
       </Route>
       <Route>
         <ErrorPage />
