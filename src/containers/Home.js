78230c99e14658ad0ea59c280eaaee9622bd2545
sriver7 <rsonny89@gmail.com>
2021-03-10 21:34:35 -0500

    Adding View Customers as Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import {useAppContext} from "../libs/contextLib";
+import {onError} from "../libs/errorLib";
 import "./Home.css";
 
 export default function Home() {
-  return (
-    <div className="Home">
-      <div className="lander">
-        <h1>RML CRM Application</h1>
-        <p className="text-muted">Customer Managment</p>
-      </div>
-    </div>
-  );
+    const [customers, setCustomers] = useState([]);
+    const {isAuthenticated} = useAppContext();
+    const [isLoading, setIsLoading] = useState(true);
+
+    function renderCustomersList(customers) {
+        return null;
+    }
+
+    function renderLander(){
+        return (
+            <div className="lander">
+                <h1>RML CRM Application</h1>
+                <p className="text-muted">Customer Managment Application</p>
+            </div>
+        );
+    }
+
+    function rednerCustomers(){
+        return (
+            <div className="customers">
+                <h2 className="pb-3 mt-4 mb-3 border-bottom">Customers List</h2>
+                <ListGroup>{!isLoading && renderCustomersList(customers)}</ListGroup>
+            </div>  
+        );
+    }
+
+    return (
+        <div className="Home">
+            {isAuthenticated ? rednerCustomers():renderLander()}
+        </div>
+    );
 }
